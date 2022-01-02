14c65bf6860b313d2398e133a303fa56bc4d5aec
Fernando <35590251+FernandoGuardado@users.noreply.github.com>
2022-01-02 00:18:00 -0700

    Added cash match pot

--- src/CallOfDuty/bot.js
+++ src/CallOfDuty/bot.js
@@ -6,13 +6,18 @@ import request from "request";
 import dotenv from "dotenv/config";
 import fs from "fs";
 
+// TODO add error checks for undefined CMG
+// TODO add firebase (firestore) for data; users, cod_data, everything... !!!
+// TODO move twitch bot config to own file; i.e. make an app/index.js
+// TODO try and import cod folder through an index.js
+
 // twitch bot config
 const config = {
   identity: {
     username: process.env.BOT_USERNAME,
     password: process.env.OAUTH_TOKEN,
   },
-  channels: ["kriptxnic", "omnibal", "yxgster", "daunttt", "tuttuhl"],
+  channels: ["kriptxnicbot"],
 };
 
 // create bot client
@@ -41,7 +46,7 @@ function onMessageHandler(channel, tags, msg, self) {
 }
 
 function match(channel, self) {
-  const rawData = fs.readFileSync("src/CallOfDuty/cod_data.json");
+  const rawData = fs.readFileSync("build/CallOfDuty/cod_data.json");
   const user_data = JSON.parse(rawData).Users[channel];
   const CMG = user_data.CMG;
 
@@ -78,6 +83,7 @@ function match(channel, self) {
                   `https://www.checkmategaming.com/${match}`
                 );
                 const match_body = await match_response.text();
+                let $ = cheerio.load(match_body);   // target match body with cheerio
 
                 // check to see if player is in the match
                 if (!match_body.includes(CMG.replace(/[0-9]/g, ""))) {
@@ -89,8 +95,11 @@ function match(channel, self) {
                   return;
                 }
 
-                // target match body with cheerio
-                let $ = cheerio.load(match_body);
+                // check to see if cash match
+                if (match_body.toLowerCase().includes("cash")) {
+                  var cash_match = $(".match-details-pool").text();
+                  cash_match = cash_match.substring(0, cash_match.indexOf(" ")).replace("Total", "").replace(/\s/g, "");
+                }
 
                 // get map list
                 var map_list = [];
@@ -105,33 +114,56 @@ function match(channel, self) {
                       .replace("Search and Destroy:", "")
                       .replace("Host", "")
                       .split(":")[0];
-                    // map_list[i] = map_list[i].split(' ')[3].replace('Host:', '');
                   } else if (map_list[i].includes("Hardpoint")) {
                     map_list[i] = map_list[i]
                       .replace("Hardpoint:", "")
                       .replace("Host", "")
                       .split(":")[0];
-                    // map_list[i] = map_list[i].split(' ')[1].replace('Host:', '');
                   }
                 }
                 if (map_list.length > 1) {
-                  var match_message = "";
-                  for (let i = 0; i < map_list.length; i++) {
-                    match_message += `Map ${i + 1}: ${map_list[i]} | `;
-                  }
-                  match_message += `checkmategaming.com${match}`;
-                  client.say(channel, match_message).catch(function (err) {
-                    console.log(err);
-                  });
-                } else {
-                  client
-                    .say(
-                      channel,
-                      `Best of 1: ${map_list[0]} | checkmategaming.com${match}`
-                    )
-                    .catch(function (err) {
+                  if (cash_match) {
+                    var match_message = `${cash_match} pool | `;
+                    for (let i = 0; i < map_list.length; i++) {
+                      match_message += `Map ${i + 1}: ${map_list[i]} | `;
+                    }
+                    match_message += `checkmategaming.com${match}`;
+
+                    client.say(channel, match_message).catch(function (err) {
                       console.log(err);
                     });
+                  } else {
+                    var match_message = "";
+                    for (let i = 0; i < map_list.length; i++) {
+                      match_message += `Map ${i + 1}: ${map_list[i]} | `;
+                    }
+                    match_message += `checkmategaming.com${match}`;
+
+                    client.say(channel, match_message).catch(function (err) {
+                      console.log(err);
+                    });
+                  }
+                } else {
+                  if (cash_match) {
+                    client
+                      .say(
+                        channel,
+                        `${cash_match} pot Bo1: ${map_list[0]} | checkmategaming.com${match}`
+                      )
+                      .catch(function (err) {
+                        console.log(err);
+                      });
+                  }
+                  else {
+                    client
+                      .say(
+                        channel,
+                        `Bo1: ${map_list[0]} | checkmategaming.com${match}`
+                      )
+                      .catch(function (err) {
+                        console.log(err);
+                      });
+                  }
                 }
               };
               getMatchDetails();
